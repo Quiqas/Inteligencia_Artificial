56344b00f56326a4c8f88a384962b10d106cc6ca
Cristhian <albert502@outlook.com>
2018-04-04 18:03:38 -0600

    Se creo una clase neurona y varias clases en el main

--- PerceptronMultiCapa/PerceptronMultiCapa/Program.cs
+++ PerceptronMultiCapa/PerceptronMultiCapa/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PerceptronMultiCapa
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace PerceptronMultiCapa{
+
+    class Neurona{
+        //Pesos para cada entrada P1 y P2; y el peso de la entrada interna U
+        private double P1;
+        private double P2;
+        private double U;
+        public Neurona(Random azar) { //Constructor
+            P1 = azar.NextDouble();
+            P2 = azar.NextDouble();
+            U = azar.NextDouble();
+        }
+        public double calculaSalida(double E1, double E2){
+            double S = 0;
+            //Se hace una operación aquí
+            return S;
+        }
+    }
+
+
+    class Program{
+        static void Main(string[] args){
+            Random azar = new Random(); //Un solo generador
+            Neurona algunaCapasOcultas = new Neurona(azar);
+            Neurona algunaCapaSalida = new Neurona(azar);
         }
     }
 }
